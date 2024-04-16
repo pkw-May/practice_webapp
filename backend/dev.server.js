84cd0b63605127651631b158810a6d79d578cf3a
pkw-May <pkw.may@gmail.com>
2024-04-17 03:58:49 +0900

    files for build

---
+++ backend/dev.server.js
@@ -0,0 +1,34 @@
+require('dotenv').config({ path: '.env.local' });
+const path = require('path');
+
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./src/routes/authRoutes');
+const postsRoutes = require('./src/routes/postsRoutes');
+const commentsRoutes = require('./src/routes/commentsRoutes');
+const PORT = process.env.PORT || 8090;
+
+const app = express();
+
+app.use(cors());
+// let corsOptions = {
+// 	origin: 'https://www.domain.com',
+// 	credentials: true
+// }
+// app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postsRoutes);
+app.use('/api/comments', commentsRoutes);
+
+// React Server
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is Running on port ${PORT}`);
+});
--- backend/src/services/authPlatforms/googleOAuthService.js
+++ backend/src/services/authPlatforms/googleOAuthService.js
@@ -1,12 +1,12 @@
-const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const client = new OAuth2Client(CLIENT_ID);
+// const { OAuth2Client } = require('google-auth-library');
+// const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// const client = new OAuth2Client(CLIENT_ID);
 
-exports.verifyGoogleToken = async (token) => {
-	const ticket = await client.verifyIdToken({
-		idToken: token,
-		audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
-	});
-	const payload = ticket.getPayload();
-	return payload;
-};
+// exports.verifyGoogleToken = async (token) => {
+// 	const ticket = await client.verifyIdToken({
+// 		idToken: token,
+// 		audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+// 	});
+// 	const payload = ticket.getPayload();
+// 	return payload;
+// };
